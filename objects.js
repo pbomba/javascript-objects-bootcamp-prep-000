91de30470202fcd78496205d82e21baf4f50182c
Paul Bomba` <paul@paulbomba.com>
2018-05-07 17:53:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
 }
 
-var name = "Prints"
-var song = ""
+var name = "Prints";
+var song = "Purple Rain";
 
 function updatePlaylist(playlist, name, song){
   Object.assign(playlist, {name: song})
