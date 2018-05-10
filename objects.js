838ae009cd52f89292a2b42fb8d245a1b9ad9166
Paul Bomba` <paul@paulbomba.com>
2018-05-10 16:08:43 +0000

    automatically backed up by learn

--- objects.js
+++
@@ -1,8 +0,0 @@
-var playlist = {"Jamiroquai" : "Cosmic Girl"};
-
-//var artistName = "Prints";
-//var songTitle = "Purple Rain";
-
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, {artistName: songTitle})
-}
