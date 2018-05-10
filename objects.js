98efa40b674c7f894686b4688d02f732640565e0
Paul Bomba` <paul@paulbomba.com>
2018-05-10 16:31:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { Jamiroquai : "Cosmic Girl" };
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign( playlist, { [artistName] : songTitle } );
 }
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+    
+}
